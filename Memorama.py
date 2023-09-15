e88d8f60dfdf6756a3b0830a908f5d0bd6f6b964
KevinAleja <A01711063@tec.mx>
2023-09-14 20:09:03 -0600

    Adding the counter to file Memorama.py

--- Memorama.py
+++ Memorama.py
@@ -11,17 +11,19 @@ Exercises:
 
 from random import *
 from turtle import *
-
 from freegames import path
 
+# Load the car image (for the background).
 car = path('car.gif')
+
+# Create a list of 32 pairs of numbers from 0 to 31 to represent the game tiles.
 tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
+tap_count = 0  # Variable to count taps, initialized to 0
 
-
+# Function to draw a square at the position (x, y).
 def square(x, y):
-    """Draw white square with black outline at (x, y)."""
     up()
     goto(x, y)
     down()
@@ -32,19 +34,24 @@ def square(x, y):
         left(90)
     end_fill()
 
-
+# Function to convert coordinates (x, y) into a tile index.
 def index(x, y):
-    """Convert (x, y) coordinates to tiles index."""
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
 
-
+# Function to convert a tile index into coordinates (x, y).
 def xy(count):
-    """Convert tiles count to (x, y) coordinates."""
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
 
+# Function to update and display the tap count.
+def update_tap_count():
+    up()
+    goto(160, -190)
+    color('black')
+    write(f'Taps: {tap_count}', align='center', font=('Arial', 16, 'normal'))
 
+# Function to handle the click event on a tile.
 def tap(x, y):
-    """Update mark and hidden tiles based on tap."""
+    global tap_count
     spot = index(x, y)
     mark = state['mark']
 
@@ -54,10 +61,11 @@ def tap(x, y):
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
+        tap_count += 1
+        update_tap_count()
 
-
+# Function to draw the car image and the game tiles.
 def draw():
-    """Draw image and tiles."""
     clear()
     goto(0, 0)
     shape(car)
@@ -77,11 +85,13 @@ def draw():
         color('black')
         write(tiles[mark], font=('Arial', 30, 'normal'))
 
+    update_tap_count()  # Update the counter in each frame
     update()
     ontimer(draw, 100)
 
-
 shuffle(tiles)
+
+# Set up the game and draw the initial interface.
 setup(420, 420, 370, 0)
 addshape(car)
 hideturtle()
